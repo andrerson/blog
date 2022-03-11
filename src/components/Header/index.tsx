911ad81ec4910d2d1f741c596d5b173996de90c2
andrerson <andrerson@tijucaalimentos.com>
2022-03-11 17:12:31 -0300

    feat: iniciando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import styles from './header.module.scss';
+
 export default function Header() {
   // TODO
+  return (
+    <div className={styles.content}>
+      <div>
+        <img src="Logo.png" alt="alt" />
+      </div>
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +25,22 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return(
+    <>
+      <Header />
+      
+      <div>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+        <div>
+          <time>15 Mar 2021</time>
+          <label> Joseph Oliveira </label>
+        </div>
+      </div>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
